9dbe58a10be6c46497598935d4cffb2704704802
MNasirulUmam <nasirul250400@gmail.com>
2021-07-01 00:19:24 +0700

    add css and js, add css, and edit aktif state

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
-</head>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap-grid.css" rel="stylesheet">
+    <link href="css/bootstrap-grid.main.css" rel="stylesheet">
+    <link href="css/bootstrap-grid.main.css.map" rel="stylesheet">
+    <link href="css/bootstrap-reboot.css" rel="stylesheet"> -->
+    </head>
 <body>
     @include('layout.nav')
     @yield('content')
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">PPDB</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <li class="nav-item{{ request()->is('/') ? 'active' : '' }}">
+          <a class="nav-link" aria-current="page" href="/">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item{{ request()->is('login') ? 'active' : '' }}">
           <a class="nav-link" href="/login">login</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item{{ request()->is('register') ? 'active' : '' }}">
           <a class="nav-link" href="/register">register</a>
         </li>
       </ul>
